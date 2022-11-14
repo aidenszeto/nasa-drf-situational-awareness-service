e4adab480ea0ff26329ddf352b59337aed3b43a7
Aiden Szeto <aszeto35@gmail.com>
2022-11-14 15:57:55 -0800

    updated seed script

--- v3/tools/seedDBGeojson.py
+++ v3/tools/seedDBGeojson.py
@@ -1,5 +1,8 @@
 # First argument is the name of the geojson file
-# Scond argument is s for if zones are static
+# Second argument is s if zones are static, otherwise dynamic
+#
+# python3 seedDBGeojson.py filename s --> zones are static
+# python3 seedDBGeojson.py filename --> zones are dynamic
 
 import pymongo
 import sys
@@ -7,12 +10,14 @@ import json
 import numpy
 import math
 import matplotlib.path as mplPath
-import geopandas as gp
 
 
 CONN_STR = "mongodb+srv://aiden:bE9wTAjULtcBFz58@cluster0.o222wih.mongodb.net/?retryWrites=true&w=majority"
 ANGLES = [2*math.pi / 3, math.pi, 4*math.pi / 3, 5*math.pi / 3, 0, math.pi / 3]
 RADIUS_INC = 0.01000000
+NO_FLY_ZONE_TYPES = ["hospital", "school", "university", "industrial", "government", "public"]
+NO_FLY_ZONE_CLASSES = ["hospital", "school", "university",
+                       "library", "public_building", "fire_station", "police", "fire_station", "courthouse", ]
 
 
 def createConnection():
@@ -22,7 +27,6 @@ def createConnection():
 
 
 def generateHexagon(lat, lon, radius):
-    print((lat, lon))
     lat = float(lat)
     lon = float(lon)
     coordinates = []
@@ -46,8 +50,7 @@ def smallestCircumscribingHexagon(zone):
                 circumscribed = False
                 break
         if circumscribed == True:
-            return list(hexagon.vertices)
-            break
+            return hexagon.vertices.tolist()
         radius += RADIUS_INC
 
 
@@ -60,11 +63,20 @@ def main(file, static):
     zones = data["features"]
 
     for i in range(len(zones)):
+        if zones[i]["geometry"]["type"] == "MultiPolygon":
+            continue
+
+        active = False
+        if "type" in zones[i]["properties"] and zones[i]["properties"]["type"] in NO_FLY_ZONE_TYPES:
+            active = True
+        if zones[i]["properties"]["fclass"] in NO_FLY_ZONE_CLASSES:
+            active = True
+
         properties = {
             "EVENT": zones[i]["properties"]["name"],
             "GUID": zones[i]["properties"]["osm_id"],
             "TYPE": zones[i]["properties"]["code"],
-            "active": 1,
+            "active": 1 if active else 0,
             "class": zones[i]["properties"]["fclass"],
             "static": 1 if static else 0
         }
@@ -72,16 +84,11 @@ def main(file, static):
             "type": "Polygon",
             "coordinates": [smallestCircumscribingHexagon(zones[i])]
         }
-        # print({
-        #     "type": "Feature",
-        #     "geometry": geometry,
-        #     "properties": properties
-        # })
-        # arizona.insert_one({
-        #     "type": "Feature",
-        #     "geometry": geometry,
-        #     "properties": properties
-        # })
+        arizona.insert_one({
+            "type": "Feature",
+            "geometry": geometry,
+            "properties": properties
+        })
 
 
 if __name__ == '__main__':
